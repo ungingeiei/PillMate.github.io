913d4a12e9dbdcf19b0015011d6a348e1bd8baca
ungingeiei <pranaya130148@gmail.com>
2025-10-12 14:36:58 +0700

    finish setup

---
+++ arduino/C9ACC1E7852AEF9A8D2E0690EE1B760ECD4D864E_survoOnTime.ino
@@ -0,0 +1,118 @@
+/*
+ *
+ * This Arduino UNO R4 code is made available for public use without any restriction
+ *
+ */
+
+#include <WiFiS3.h>
+#include <MQTTClient.h>
+#include <Servo.h>
+
+Servo drop;
+
+const int BUZZER = 0;
+
+const char WIFI_SSID[] = "iPhone";     // CHANGE TO YOUR WIFI SSID
+const char WIFI_PASSWORD[] = "nnnnnnnn";  // CHANGE TO YOUR WIFI PASSWORD
+
+const char MQTT_BROKER_ADRRESS[] = "mqtt-dashboard.com";  // CHANGE TO MQTT BROKER'S ADDRESS
+//const char MQTT_BROKER_ADRRESS[] = "192.168.0.11";  // CHANGE TO MQTT BROKER'S IP ADDRESS
+//const int MQTT_PORT = 8884; // website
+const int MQTT_PORT = 1883; // arduino
+const char MQTT_CLIENT_ID[] = "clientId-MediGroup";  // CHANGE IT AS YOU DESIRE
+const char MQTT_USERNAME[] = "";              // CHANGE IT IF REQUIRED, empty if not required
+const char MQTT_PASSWORD[] = "";              // CHANGE IT IF REQUIRED, empty if not required
+
+// The MQTT topics that Arduino should publish/subscribe
+const char SUBSCRIBE_TOPIC[] = "PillMate/20";  // CHANGE IT AS YOU DESIRE
+
+const int PUBLISH_INTERVAL = 5000;  // 5 seconds
+
+WiFiClient network;
+MQTTClient mqtt = MQTTClient(256);
+
+unsigned long lastPublishTime = 0;
+
+void setup() {
+  Serial.begin(9600);
+  drop.attach(9);
+  drop.write(180);
+  pinMode(BUZZER , OUTPUT);
+
+  int status = WL_IDLE_STATUS;
+  while (status != WL_CONNECTED) {
+    Serial.print("Arduino UNO R4 - Attempting to connect to SSID: ");
+    Serial.println(WIFI_SSID);
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    status = WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+    // wait 10 seconds for connection:
+    delay(10000);
+  }
+  // print your board's IP address:
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  connectToMQTT();
+}
+
+void loop() {
+  mqtt.loop();
+
+  if (millis() - lastPublishTime > PUBLISH_INTERVAL) {
+    lastPublishTime = millis();
+  }
+}
+
+void connectToMQTT() {
+  // Connect to the MQTT broker
+  mqtt.begin(MQTT_BROKER_ADRRESS, MQTT_PORT, network);
+
+  // Create a handler for incoming messages
+  mqtt.onMessage(messageReceived);
+
+  Serial.print("Arduino UNO R4 - Connecting to MQTT broker");
+
+  while (!mqtt.connect(MQTT_CLIENT_ID, MQTT_USERNAME, MQTT_PASSWORD)) {
+    Serial.print(".");
+    delay(100);
+  }
+  Serial.println();
+
+  if (!mqtt.connected()) {
+    Serial.println("Arduino UNO R4 - MQTT broker Timeout!");
+    return;
+  }
+
+  // Subscribe to a topic, the incoming messages are processed by messageHandler() function
+  if (mqtt.subscribe(SUBSCRIBE_TOPIC))
+    Serial.print("Arduino UNO R4 - Subscribed to the topic: ");
+  else
+    Serial.print("Arduino UNO R4 - Failed to subscribe to the topic: ");
+
+  Serial.println(SUBSCRIBE_TOPIC);
+  Serial.println("Arduino UNO R4 - MQTT broker Connected!");
+}
+
+void messageReceived(String &topic, String &payload) {
+  Serial.println("Arduino UNO R4 - received from MQTT:");
+  Serial.println("- topic: " + topic);
+  Serial.println("- payload:");
+  Serial.println(payload);
+
+  if(payload == "ถึงเวลากินยาแล้ว💊"){
+    drop.write(0);
+    delay(1000);
+    drop.write(180);
+    delay(1000);
+    digitalWrite(BUZZER , HIGH);
+    delay(1000);
+    digitalWrite(BUZZER , LOW);
+    delay(1000);
+  }
+
+  // You can process the incoming data , then control something
+  /*
+  process something
+  */
+}
